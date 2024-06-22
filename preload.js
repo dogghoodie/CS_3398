f3c5bb8f17b3b380c5f2173b12a7ce42fd13bbe0
dogghoodie <dogghoodie@gmail.com>
2024-06-21 21:17:50 -0500

    Modified preload.js to handle the file list.

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  concatVideos: (file1, file2, output) => ipcRenderer.invoke('concat-videos', file1, file2, output),
+  concatVideos: (file1, file2, output) => ipcRenderer.invoke('concat-videos', files, output),
   queryFiles: (dirPath, formats) => ipcRenderer.invoke('query-files', dirPath, formats),
   getCore: () => ipcRenderer.invoke('get-core'),
   setCore: (newCore) => ipcRenderer.invoke('set-core', newCore),
